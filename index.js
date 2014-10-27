ddfd64b534a1b2f36bdb64dc8e299d5db839e081
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-27 14:58:41 +1100

    Added functionality to attempt connecting to multiple endpoints

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var WebSocket = require('ws');
 var wsurl = require('wsurl');
 var ps = require('pull-ws');
 var defaults = require('cog/defaults');
+var reTrailingSlash = /\/$/;
 
 /**
   # messenger-ws
@@ -9,12 +10,18 @@ var defaults = require('cog/defaults');
   This is a simple messaging implementation for sending and receiving data
   via websockets.
 
+  Follows the [messenger-archetype](https://github.com/DamonOehlman/messenger-archetype)
+
   ## Example Usage
 
   <<< examples/simple.js
 
 **/
 module.exports = function(url, opts) {
+  var timeout = (opts || {}).timeout || 1000;
+  var endpoints = ((opts || {}).endpoints || ['/']).map(function(endpoint) {
+    return url.replace(reTrailingSlash, '') + endpoint;
+  });
 
   // set defaults (close on end = true)
   opts = defaults({}, opts, {
@@ -22,11 +29,27 @@ module.exports = function(url, opts) {
   });
 
   function connect(callback) {
-    var socket = new WebSocket(wsurl(url));
+    var queue = [].concat(endpoints);
+    var timer;
 
-    socket.addEventListener('open', function() {
-      callback(null, ps.source(socket, opts), ps.sink(socket, opts));
-    });
+    function attemptNext() {
+      var socket;
+
+      // if we have no more valid endpoints, then erorr out
+      if (queue.length === 0) {
+        return callback(new Error('Unable to connect to url: ' + url));
+      }
+
+      socket = new WebSocket(wsurl(queue.shift()));
+      socket.addEventListener('open', function() {
+        clearTimeout(timer);
+        callback(null, ps.source(socket, opts), ps.sink(socket, opts));
+      });
+
+      timer = setTimeout(attemptNext, timeout);
+    }
+
+    attemptNext();
   }
 
   return connect;
