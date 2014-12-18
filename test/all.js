11d73a4c6c90eb3ab907217d91e24895c7abb0ac
Damon Oehlman <damon.oehlman@gmail.com>
2014-12-18 16:16:04 +1100

    Added a timeout to allow the server time to start

--- test/all.js
+++ test/all.js
@@ -1,3 +1,10 @@
+var test = require('tape');
+
+test('wait for the server to be ready', function(t) {
+  t.plan(1);
+  setTimeout(t.pass, 1000);
+});
+
 require('./read');
 require('./echo');
 require('./endpoint-failover');
