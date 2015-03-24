229f96bae7f1aabd575a23c55e21e19ce8ced05f
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-24 15:19:24 +1100

    Refactor server to be compatible with broth

--- test/server.js
+++ test/server.js
@@ -1,50 +1,56 @@
+var http = require('http');
 var WebSocketServer = require('ws').Server;
 var mapleTree = require('mapleTree');
-var port = process.env.ZUUL_PORT || process.env.PORT || 3000;
-var wss = module.exports = new WebSocketServer({ port: port });
-var router = new mapleTree.RouteTree();
 
-router.define('/read', function(ws) {
-  var values = ['a', 'b', 'c', 'd'];
-  var timer = setInterval(function() {
-    var next = values.shift();
-    if (next) {
-      ws.send(next);
-    }
-    else {
-      clearInterval(timer);
-      ws.close();
-    }
-  }, 100);
-});
+module.exports = function() {
+  var server = http.createServer();
+  var wss = new WebSocketServer({ server: server });
+  var router = new mapleTree.RouteTree();
 
-router.define('/echo', function(ws) {
-  ws.on('message', function(data) {
-    console.log('received message: ', data);
-    ws.send(data);
+  router.define('/read', function(ws) {
+    var values = ['a', 'b', 'c', 'd'];
+    var timer = setInterval(function() {
+      var next = values.shift();
+      if (next) {
+        ws.send(next);
+      }
+      else {
+        clearInterval(timer);
+        ws.close();
+      }
+    }, 100);
   });
-});
 
-function abortConnection(socket, code, name) {
-  try {
-    var response = [
-      'HTTP/1.1 ' + code + ' ' + name,
-      'Content-type: text/html'
-    ];
-    socket.write(response.concat('', '').join('\r\n'));
-  }
-  catch (e) { /* ignore errors - we've aborted this connection */ }
-  finally {
-    // ensure that an early aborted connection is shut down completely
-    try { socket.destroy(); } catch (e) {}
-  }
-}
+  router.define('/echo', function(ws) {
+    ws.on('message', function(data) {
+      console.log('received message: ', data);
+      ws.send(data);
+    });
+  });
 
-wss.on('connection', function(ws) {
-  var match = router.match(ws.upgradeReq.url);
-  if (match && typeof match.fn == 'function') {
-    return match.fn(ws);
+  function abortConnection(socket, code, name) {
+    try {
+      var response = [
+        'HTTP/1.1 ' + code + ' ' + name,
+        'Content-type: text/html'
+      ];
+      socket.write(response.concat('', '').join('\r\n'));
+    }
+    catch (e) { /* ignore errors - we've aborted this connection */ }
+    finally {
+      // ensure that an early aborted connection is shut down completely
+      try { socket.destroy(); } catch (e) {}
+    }
   }
 
-  ws.close(1002, 'Not found');
-});
+  wss.on('connection', function(ws) {
+    var match = router.match(ws.upgradeReq.url);
+    if (match && typeof match.fn == 'function') {
+      return match.fn(ws);
+    }
+
+    ws.close(1002, 'Not found');
+  });
+
+  return server;
+};
